98ec977c1288f9f0a11d49f486658cd66c86f7d9
Bernardo Costa <bfvcc01@gmail.com>
2023-07-24 10:39:26 -0300

    fina43

--- js/js.js
+++ js/js.js
@@ -1,22 +1,46 @@
 const btns = document.querySelectorAll(".btn");
 
+function addErro(input,msg){
+  const div = input.parentElement
+  const inputSelect = div.querySelector('input')
+  const spanElement = document.createElement('span')
+  const span = div.querySelector('.erroText');
+  if(span){
+    return
+  }
+    spanElement.innerText = msg
+    spanElement.classList.add('erroText')
+    if(div.dataset.js === 'input-header'){
+      spanElement.classList.add('absoluteItem')
+      div.insertAdjacentElement('beforeend',spanElement)
+      return
+    }
+    inputSelect.insertAdjacentElement('afterend',spanElement)
+}
+
+function removeMensagemErro(){
+  const hasErro = document.querySelector('.erroText')
+  if(hasErro){
+      hasErro.remove()
+  }
+}
+
 
 btns.forEach((btn) => {
   btn.addEventListener("click", (event) => {
     event.preventDefault();
-    const input = event.target.previousElementSibling.previousElementSibling;
-    const spanErro = event.target.previousElementSibling
-    console.log(spanErro);
+    const conteinerForm = event.currentTarget.parentElement
+    const input = conteinerForm.querySelector('input')
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const isEmpty = input.value.trim();
     const isValidEmail = emailRegex.test(input.value.trim());
 
     if (isValidEmail && isEmpty) {
         input.classList.remove("error");
-        spanErro.classList.add('remove')
+        removeMensagemErro()
     } else {
         input.classList.add("error");
-        spanErro.classList.remove('remove')
+        addErro(input,'Please check your email')
     }
   });
 });
